9aca314ad8dd1cbb0c31e6e76f2362db0b572326
Abhishek Goud <abhigoud198484@gamil.com>
2024-07-18 01:15:04 +0530

    Add New Folder

---
+++ Car_Rental_System/Rental/Rental.java
@@ -0,0 +1,27 @@
+package Car_Rental_System.Rental;
+import Car_Rental_System.Car.Car;
+import Car_Rental_System.Customer.Customer;
+
+ public class Rental {
+    private Car car;
+    private Customer customer;
+    private int days;
+
+    public Rental(Car car, Customer customer, int days) {
+        this.car = car;
+        this.customer = customer;
+        this.days = days;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public int getDays() {
+        return days;
+    }
+}
